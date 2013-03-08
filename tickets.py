149dbb0db8e76a297be888d272931583fb064516
Jake Dwyer <mjdwyer@gmail.com>
2013-03-08 12:42:31 -0800

    test gmail

--- tickets.py
+++ tickets.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+
+import cgi, cgitb
+from urllib2 import urlopen
+from json import load, dumps
 import imaplib, getpass, email, os, re
 from datetime import datetime, date, time
 
-user = raw_input('enter email: ')
-pwd = raw_input('enter password: ')
+#cgitb.enable()
 
+
+form = cgi.FieldStorage() 
+
+#user = raw_input('enter email: ')
+#pwd = raw_input('enter password: ')
+
+user = form.getvalue('user')
+pwd = form.getvalue('pwd')
 
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
 mail.login(user, pwd)
@@ -21,11 +36,6 @@ def extract_body(payload):
 resp, items = mail.search(None, '(FROM "customer_support@ticketmaster.com" SUBJECT "Ticket Order")')
 items = items[0].split()
 
-my_msg = []
-artist = []
-venue = []
-date_of_show = []
-city = []
 concert = []
 
 for emailid in items[::-1]:
@@ -37,27 +47,26 @@ for emailid in items[::-1]:
 			subject=msg['subject']
 			payload=msg.get_payload()
 			body=extract_body(payload)
-			#print subject + "\n"
 			event = body.split(' to:\r\n\r\n')
-			#print event[0]
 			event = event[1].split('Order for:')
 			event_data = event[0]
-			#print event_data
 			occurs = event_data.split('\r\n')[3]
 			raw_date = datetime.strptime(occurs[:-4], "%A, %B %d, %Y %I:%M %p")
 			artist = event_data.split('\r\n')[0]
 			venue = event_data.split('\r\n')[1]
 			city = event_data.split('\r\n')[2]
 			if raw_date.date() > date.today():
-				#artist = event_data.split('\r\n')[0]
-				#venue = event_data.split('\r\n')[1]
-				#city = event_data.split('\r\n')[2]
-				#date_of_show.append(raw_date)
-				#artist.append(event_data.split('\r\n')[0])
-				#venue.append(event_data.split('\r\n')[1])
-				#city.append(event_data.split('\r\n')[2])
 				date_of_show = raw_date.strftime("%A, %B %d %Y at %I:%M %p")
-				concert.append("<b>" + artist + "</b><br />" + venue + "<br />" + city + "<br />" + date_of_show + "<br />")
-
+				concert.append("<p><b>" + artist + "</b><br />" + venue + "<br />" + city + "<br />" + date_of_show + "<br />")
+				
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>Your Upcoming Shows</title>"
+print "</head>"
+print "<body>"
+print "<h2>Your Upcoming Shows</h2>" 
 for show in concert:
 	print show
+print "</body>"
+print "</html>"
